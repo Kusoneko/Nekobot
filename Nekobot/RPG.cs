22c0e09c1ff896634046a9d49026040f0454c4f6
Lirusaito <inusaito@gmail.com>
2016-01-31 19:26:36 -0500

    Initial `roll` rewrite
    TODO: Add +/-mod feature... just gotta get that working right.

--- Nekobot/RPG.cs
+++ Nekobot/RPG.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord;
 
 namespace Nekobot
 {
     static class RPG
     {
+        static int[] RollValues(GroupCollection groups = null)
+        {
+            int times = groups != null && groups[1].Length != 0 ? int.Parse(groups[1].Value) : 1;
+            int count = groups != null && groups[2].Length != 0 ? int.Parse(groups[2].Value) : 1;
+            int sides = groups != null && groups[3].Length != 0 ? int.Parse(groups[3].Value) : 6;
+            return new[]{ times, count, sides };
+        }
+        static async Task DoRoll(Channel channel, int[] t)
+        {
+            int times = t[0], count = t[1], sides = t[2];
+            string response = (count <= 0 || sides <= 1 || times <= 0) ? $"{sides*times*count}, baka!" : "";
+            if (!response.Any())
+            {
+                var total = 0;
+                Random rnd = new Random();
+                for (int i = times; i > 0; i--)
+                {
+                    var subtotal = 0;
+                    for (int j = count; j > 0; j--)
+                    {
+                        var roll = rnd.Next(1, sides + 1);
+                        if (count > 1) response += $"{roll}{(j == 1 ? "" : ", ")}";
+                        subtotal += roll;
+                    }
+
+                    if (times > 1) response += count > 1 ? $" = {subtotal}.\n" : $"{subtotal}{(i == 1 ? "" : ", ")}";
+                    total += subtotal;
+                }
+                response += $"{(times == 1 || count == 1 ? "" : "Total Result")} = {total}.";
+            }
+            await channel.SendMessage(response);
+        }
+
         internal static void AddCommands(Commands.CommandGroupBuilder group)
         {
             group.CreateCommand("rand")
@@ -39,45 +75,31 @@ namespace Nekobot
                 });
 
             group.CreateCommand("roll")
-                .Parameter("dice", Commands.ParameterType.Optional)
-                .Parameter("sides", Commands.ParameterType.Optional)
-                .Parameter("times", Commands.ParameterType.Optional)
-                .Description("I'll roll a few sided dice for a given number of times. All params are optional. (defaults: 1 *dice*, 6 *sides*, 1 *times*)")
+                .Parameter("[times] [count]d[sides]...", Commands.ParameterType.Multiple)
+                .Description("I'll roll `count` `sides` sided dice and add each `mod` to the result `times`. All params are optional. (defaults: 1 *dice*, 6 *sides*, 1 *times*)\nyou can batch roll with different params if you repeat the arguments.")
                 .Do(async e =>
                 {
-                    if (e.Args.Length != 0 && e.Args[0] == "rick")
+                    var chan = e.Channel;
+                    if (e.Args.Length == 0) // Default roll.
                     {
-                        await e.Channel.SendMessage("https://youtu.be/dQw4w9WgXcQ");
+                        await DoRoll(chan, RollValues());
+                        return;
+                    }
+                    if (e.Args[0].ToLower() == "rick")
+                    {
+                        await chan.SendMessage("https://youtu.be/dQw4w9WgXcQ");
                         return;
                     }
 
-                    try
+                    MatchCollection m = !e.Args.Any() ? null : Regex.Matches(string.Join(" ", e.Args), "(?:([0-9]*) ){0,1}([0-9]*){0,1}d([0-9]*)");
+                    if (m.Count == 0)
+                        await chan.SendMessage("Incorrect Argument Syntax!");
+                    else
                     {
-                        int dice = e.Args.Length >= 1 ? int.Parse(e.Args[0]): 1;
-                        int sides = e.Args.Length >= 2 ? int.Parse(e.Args[1]): 6;
-                        int times = e.Args.Length >= 3 ? int.Parse(e.Args[2]): 1;
-
-                        string response = (dice <= 0 || sides <= 1 || times <= 0) ? $"{sides*times*dice}, baka!" : "";
-                        if (!response.Any())
-                        {
-                            var total = 0;
-                            Random rnd = new Random();
-                            for (int i = times; i > 0; i--)
-                            {
-                                var subtotal = 0;
-                                for (int j = dice; j > 0; j--)
-                                {
-                                    var roll = rnd.Next(1, sides + 1);
-                                    if (dice > 1) response += $"{roll}{(j == 1 ? "" : ", ")}";
-                                    subtotal += roll;
-                                }
-                                if (times > 1) response += dice > 1 ? $" = {subtotal}.\n" : $"{subtotal}{(i == 1 ? "" : ", ")}";
-                                total += subtotal;
-                            }
-                            response += $"{(times == 1 || dice == 1 ? "" : "Total Result")} = {total}.";
-                        }
-                        await e.Channel.SendMessage(response);
-                    } catch { await e.Channel.SendMessage("Arguments are not all numbers!"); }
+                        foreach (var groups in from Match match in m select match.Groups)
+                            if (groups[0].Length != 0)
+                                await DoRoll(chan, RollValues(groups));
+                    }
                 });
         }
     }
